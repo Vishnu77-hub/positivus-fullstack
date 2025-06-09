a1688d22a87c6f850540a46dea7cb2d353b9f19b
Vishnu77-hub <vishnu1805j@gmail.com>
2025-06-10 03:00:50 +0530

    confirmed frontend link

--- admin/src/pages/pricingpages/pricingtables/PricingTable.jsx
+++ admin/src/pages/pricingpages/pricingtables/PricingTable.jsx
@@ -28,6 +28,7 @@ const PricingTable = ({ token }) => {
 // fetch data from backend -------------------------------------------------------
   const fetchListPricing = async () => {
     try {
+      const token = localStorage.getItem("token");
       const { data } = await axios.get(`${backendUrl}/api/pricing/listPricingTable`);
       if (data.success) setListPricing(data.pricing);
       else toast.error(data.message);
@@ -40,6 +41,7 @@ const PricingTable = ({ token }) => {
 // Enavle/Disable -------------------------------------------------------------------------------
   const toggleEnable = async (id, enableStatus) => {
     try {
+      const token = localStorage.getItem("token");
       const { data } = await axios.put(
         `${backendUrl}/api/pricing/togglePricingEnable`,
         { id, isEnabledPricing: enableStatus },
@@ -62,6 +64,7 @@ const PricingTable = ({ token }) => {
   // remove data ----------------------------------------------------------------------------------------------------
   const removePricing = async (id) => {
     try {
+      const token = localStorage.getItem("token");
       const { data } = await axios.post(
         `${backendUrl}/api/pricing/removePricing`,
         { id },
@@ -80,6 +83,7 @@ const PricingTable = ({ token }) => {
   // Data submit -----------------------------------------------------------------------------------------
   const submitUpdate = async () => {
     try {
+      const token = localStorage.getItem("token");
       const form = new FormData();
       form.append('pricingId', editingItem._id);
       form.append('planName', editingItem.planName);
